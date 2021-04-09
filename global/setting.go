fbda5d7eef781fdf13b070691ad1778c71753e3e
jianwudi <598282570@qq.com>
2021-04-09 19:54:12 +0800

    ss

--- global/setting.go
+++ global/setting.go
@@ -1,10 +1,14 @@
 package global
 
-import "github.com/go-programming-tour/blog-service/pkg/setting"
+import (
+	"github.com/go-programming-tour/blog-service/pkg/logger"
+	"github.com/go-programming-tour/blog-service/pkg/setting"
+)
 
 //定义全局变量
 var (
 	ServerSetting   *setting.ServerSettingS
 	AppSetting      *setting.AppSettingS
 	DatabaseSetting *setting.DatabaseSettingS
+	Logger          *logger.Logger
 )
--- main.go
+++ main.go
@@ -8,15 +8,27 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-programming-tour/blog-service/global"
 	"github.com/go-programming-tour/blog-service/internal/routers"
+	"github.com/go-programming-tour/blog-service/pkg/logger"
 	"github.com/go-programming-tour/blog-service/pkg/setting"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func init() {
+	//解析配置文件
 	err := setupSetting()
 	if err != nil {
 		log.Fatalf("init.setupSetting err: %v", err)
 	}
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err: %v", err)
+	}
 }
+
+// @title 博客系统
+// @version 1.0
+// @description Go 语言编程之旅：一起用 Go 做项目
+// @termsOfService https://github.com/go-programming-tour-book
 func main() {
 	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
@@ -27,6 +39,7 @@ func main() {
 		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
+	global.Logger.Infof("%s: go-programming-tour-book/%s", "eddycjy", "blog-service")
 	s.ListenAndServe()
 }
 
@@ -36,15 +49,17 @@ func setupSetting() error {
 	if err != nil {
 		return err
 	}
-	//读取setting中信息
+	//解析端口等信息
 	err = setting.ReadSection("Server", &global.ServerSetting)
 	if err != nil {
 		return err
 	}
+	//
 	err = setting.ReadSection("App", &global.AppSetting)
 	if err != nil {
 		return err
 	}
+	//解析数据库配置信息
 	err = setting.ReadSection("Database", &global.DatabaseSetting)
 	if err != nil {
 		return err
@@ -52,5 +67,16 @@ func setupSetting() error {
 	//	fmt.Printf("%#v\n", global.ServerSetting)
 	global.ServerSetting.ReadTimeout *= time.Second
 	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupLogger() error {
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename:  global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt,
+		MaxSize:   600,
+		MaxAge:    10,
+		LocalTime: true,
+	}, "", log.LstdFlags).WithCaller(2)
+
 	return nil
 }
